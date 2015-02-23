fc6c454d72a583dc4eb23c1eacb3a0581f2106c3
harshflipk <harshwardhan.c@flipkart.com>
2015-02-24 00:46:43 +0530

    initial commit

---
+++ ApnaAspatalPatient/app/src/main/java/com/apnaaspatal/patient/activity/PatientMainActivity.java
@@ -0,0 +1,17 @@
+package com.apnaaspatal.patient.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.apnaaspatal.patient.R;
+
+/**
+ * Created by harshwardhan.c on 22/02/15.
+ */
+public class PatientMainActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.patient_register_layout);
+    }
+}
